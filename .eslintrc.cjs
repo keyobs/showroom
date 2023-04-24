747b8dd68fe68cd83c79b380a3c0c9d11ba58743
keyobs <elodie.koob@affilae.com>
2023-04-24 22:20:51 +0200

    format-linter config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,40 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
-}
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/jsx-runtime',
+        'prettier'
+    ],
+    overrides: [
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true }
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint'
+    ],
+    rules: {
+        indent: ['error', 4],
+        camelcase: 'error',
+        eqeqeq: ['warn', 'smart'],
+        quotes: ['warn', 'single'],
+        semi: ['warn', 'always'],
+        'no-multi-assign': 'error',
+        'no-var': 'warn',
+        'prefer-const': 'warn',
+        'no-duplicate-imports': 'warn',
+        'linebreak-style': ['warn', 'unix'],
+        'react/prop-types': 'off'
+    }
+};
